7de9a96d908e028c7a0e683512ad9715854b5a9e
krafairus <64279814+krafairus@users.noreply.github.com>
2023-03-08 18:21:35 -0500

    Update arkrat-install.sh

--- arkrat-install.sh
+++ arkrat-install.sh
@@ -12,12 +12,13 @@ if [ "$(id -u)" != "1000" ]; then
    exit 1
 fi
 sudo pacman -S --noconfirm --needed dialog
-items=(1 "Instalar Un Aur Helper"
-       2 "Entorno De Escritorio"
-       3 "Audio Y Video (proximamente)"
-       4 "Kernerls Linux"
-       5 "Programas Esenciales (proximamente)"        
-       6 "Salir"
+items=(1 "AUR HELPER               Gestos de Paquetes con soporte AUR (Arch User Repository)"
+       2 "(DE) O (WM)              Entorno de Escritorio O un Gestor de Ventanas"
+       3 "DRIVERS (EN DESARROLLO)  Controladores graficos, de multimedia entre otros"
+       4 "KERNELS                  Kernels linux para distintos usos"
+       5 "PROGRAMAS EXTRAS         Programas para uso comun o inicial"     
+       6 "SALIR"
+
                 )
 while choice=$(dialog --title "- | Arkrat - krafairus - krafairus.blogspot.com | -" \
                  --menu "\nSeleccione una opción a continuación utilizando las teclas\n[FLECHA ARRIBA] / [FLECHA ABAJO] y [ESPACIO] o [ENTER]\nLas teclas alternativas también se pueden usar: [+], [-] y [TAB]. \n" 20 80 10 "${items[@]}" \
